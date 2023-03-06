d0167bc503658b0a30b6401e26ba53c6cf605f82
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-06 05:23:48 -0500

    add delete Urls from DB

--- express_server.js
+++ express_server.js
@@ -6,7 +6,6 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,10 +15,15 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
 });
 
 function generateRandomString() {}
@@ -33,7 +37,6 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -46,7 +49,7 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
- 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
