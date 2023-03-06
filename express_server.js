3147edd19cd4a6b480353a486875cf653ecf9ea5
SEAF39 <labber@DESKTOP-7J1C764>
2023-03-06 00:56:12 -0500

    update files

--- express_server.js
+++ express_server.js
@@ -14,6 +14,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id].longURL 
+  };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
